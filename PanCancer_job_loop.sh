2fbdcbc114a3b1df6a509a88fdd3a79be83e6bf1
Jamie Wang <sjw4002@med.cornell.edu>
2025-01-29 10:45:46 -0500

    Best ndims PanCancer

--- PanCancer_job_loop.sh
+++ PanCancer_job_loop.sh
@@ -1,8 +1,15 @@
 #!bin/bash
 
 datasets=("BrCa1" "BrCa2" "COAD" "HCC" "PDAC" "PRAD" "RC12" "RC18" "RC20")
+ndims_list=(41 41 51 51 41 51 31 41 51)
 
-for dataset in "${datasets[@]}"
+
+for i in "${!datasets[@]}"
 do
-    bash /data1/reznike/wangs13/UnitedMet/Performance_Benchmarking/scripts/PanCancer_job "$dataset"
+    dataset=${datasets[$i]}
+    ndims=${ndims_list[$i]}
+    echo ${dataset}
+    echo ${ndims}
+
+    bash /data1/reznike/wangs13/UnitedMet/Performance_Benchmarking/scripts/PanCancer_job "$dataset" "$ndims"
 done
